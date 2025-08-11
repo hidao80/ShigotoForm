0d47a508708b89c845f03f327670d36934386f80
hidao80 <8155294+hidao80@users.noreply.github.com>
2025-08-11 12:07:35 +0900

    :sparkles: Add offline support for PWA

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,31 @@ export default {
                 'img/favicon128.webp',
                 'img/favicon256.webp',
                 'img/favicon512.webp'
-            ]
+            ],
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,woff,woff2}'],
+                navigateFallback: 'index.html',
+                cleanupOutdatedCaches: true,
+                runtimeCaching: [
+                    {
+                        urlPattern: /\.(?:png|webp|jpg|jpeg|svg|gif|ico)$/,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'images',
+                            expiration: {
+                                maxEntries: 200,
+                                maxAgeSeconds: 60 * 60 * 24 * 30
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    }
+                ]
+            },
+            devOptions: {
+                enabled: true
+            }
         })
     ]
 }
