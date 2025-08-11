9dd032a146f4b4805b1bd8b0a6c82e0fa51a866b
hidao80 <8155294+hidao80@users.noreply.github.com>
2025-08-11 12:00:46 +0900

    :sparkles: Add PWA support using Vite features

--- vite.config.js
+++ vite.config.js
@@ -6,34 +6,17 @@ export default {
         basicSsl(),
         VitePWA({
             registerType: 'autoUpdate',
-            manifest: {
-                name: 'ShigotoForm',
-                short_name: 'ShigotoForm',
-                description: 'ShigotoForm is a project designed to streamline and simplify job application processes.',
-                theme_color: '#ffffff',
-                icons: [
-                    {
-                        src: 'public/img/favicon512.webp',
-                        sizes: '512x512',
-                        type: 'image/webp'
-                    },
-                    {
-                        src: 'public/img/favicon256.webp',
-                        sizes: '256x256',
-                        type: 'image/webp'
-                    },
-                    {
-                        src: 'public/img/favicon72.webp',
-                        sizes: '72x72',
-                        type: 'image/webp'
-                    },
-                    {
-                        src: 'public/img/favicon32.webp',
-                        sizes: '32x32',
-                        type: 'image/webp'
-                    }
-                ]
-            }
+            // 静的 public/manifest.json を使用するため、ここではmanifestを定義しない
+            includeAssets: [
+                'img/favicon32.webp',
+                'img/favicon32.png',
+                'img/favicon48.webp',
+                'img/favicon64.webp',
+                'img/favicon72.webp',
+                'img/favicon128.webp',
+                'img/favicon256.webp',
+                'img/favicon512.webp'
+            ]
         })
     ]
 }
